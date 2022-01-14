8d70a427ca21eb48e5c550b60732507c9d655e2b
sumaiya006 <90856179+sumaiya006@users.noreply.github.com>
2022-01-14 18:53:16 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// var nav_holder = document.querySelector('.nav-holder');
+// var bars = document.querySelector('.fa-bars');
+
+// bars.onclick = function(){
+//     console.log("1");
+//     nav_holder.classList.toggle('show');
+// }
